70afa79034be6a897ccd01929fde01a9f1f97e3c
aixxy7 <160022160+aixxy7@users.noreply.github.com>
2024-08-06 19:21:19 +0700

    Update ins-xray.sh

--- data/ins-xray.sh
+++ data/ins-xray.sh
@@ -67,7 +67,7 @@ touch /var/log/xray/error2.log
 latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
 # Installation Xray Core
 # $latest_version
-xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v1.7.5/xray-linux-64.zip"
+xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v1.8.23/xray-linux-64.zip"
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version $latest_version
 
 ## crt xray
